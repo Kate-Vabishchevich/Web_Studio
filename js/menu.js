f33f3487b18da6c8f0a242bac5d14e3ce7dfde36
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2022-09-06 14:56:16 +0300

    поминяла скрипт

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,26 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('menu-open'),
-    closeMenuBtn: document.querySelector('menu-close'),
-    menu: document.querySelector('mob-menu'),
+    openMenuBtn: document.querySelector('.menu-open'),
+    closeMenuBtn: document.querySelector('.menu-close'),
+    menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
+    menuList: document.querySelector('.mob-menu-list'),
   };
 
-  refs.openMenulBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+    refs.body.classList.toggle('no-scroll');
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
   }
 })();
+
+
 
